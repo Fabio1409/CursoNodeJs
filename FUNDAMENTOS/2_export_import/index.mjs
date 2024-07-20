e8b47c0902493bf4b39e89ccbd97e652240936fb
Jose Fabio da Silva <fabio.7.45@hotmail.com>
2024-07-19 21:57:43 -0300

    projeto nodesJs

---
+++ FUNDAMENTOS/2_export_import/index.mjs
@@ -0,0 +1,5 @@
+// importando a funçaõ soma pelo modulo export 
+import soma from './meu_modulo.mjs'
+
+soma(23,63);
+soma(58,69);
---
+++ FUNDAMENTOS/3_core_modulo/index.js
@@ -0,0 +1,8 @@
+// me mostra qual tipo de arquivo 
+// o arquivo e exibido coma  função coreModulo
+
+const path = require('path');
+
+const extension = path.extname("arquivo.pdf")
+
+console.log(extension)
