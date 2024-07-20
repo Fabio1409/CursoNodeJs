35d5880d311d96cc51102c7f25618cb9248cc63f
Jose Fabio da Silva <fabio.7.45@hotmail.com>
2024-07-19 22:49:31 -0300

    LerArgumentos

--- FUNDAMENTOS/4_ler_argumentos/index.js
+++ FUNDAMENTOS/4_ler_argumentos/index.js
@@ -5,4 +5,12 @@ console.log(process.argv);
 const args = process.argv.slice(2);
     console.log(args)
 
-const nome = args[0]
+const nome = args[0].split("=")[1]
+
+console.log(nome)
+
+const idade = args[1].split("=")[1]
+
+console.log(idade)
+
+console.log(`O nome dele e ${nome} e ele tem ${idade} anos!`)
