fd94d562f08dd0c0a71ceee7b1e39b71b86d755e
Jose Fabio da Silva <fabio.7.45@hotmail.com>
2024-07-21 23:25:37 -0300

    event_loop sistema de execução

--- FUNDAMENTOS/8_chalk/index.js
+++ FUNDAMENTOS/8_chalk/index.js
@@ -5,6 +5,6 @@ const nota = 9;
 if (nota >= 7) {
     console.log(chalk.green.bold('Parabéns! Você está aprovado!'));
 } else {
-    console.log(chalk.bgRed('Você precisa fazer a prova de recuperação!'));
+    console.log(chalk.bgRed.black('Você precisa fazer a prova de recuperação!'));
 }
 
